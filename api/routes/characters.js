cd25642c44011f114c0a071f12ea4dba1ed2ef9b
dmychel <dmychel@tutanota.com>
2024-03-28 18:22:19 -0400

    renived the res.json because it was setting the header more than once, which is illegal

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -6,12 +6,10 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const allCharacters = await getAll(req, res, Character);
-  res.json(allCharacters);
 });
 
 router.get("/:id", async (req, res) => {
   const character = await getOne(req, res, req.params.id, Character);
-  res.json(character);
 });
 
 module.exports = router;
--- api/routes/locations.js
+++ api/routes/locations.js
@@ -6,12 +6,10 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const allLocations = await getAll(req, res, Location);
-  res.json(allLocations);
 });
 
 router.get("/:id", async (req, res) => {
   const location = await getOne(req, res, req.params.id, Location);
-  res.json(location);
 });
 
 module.exports = router;
