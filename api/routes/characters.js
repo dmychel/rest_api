e85c45d9c4ed438661492ed9860952bc1161ecb2
dmychel <dmychel@tutanota.com>
2024-03-28 17:06:09 -0400

    moved logic to module and used the module function to make the requests

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -1,32 +1,17 @@
 const express = require("express");
 const Character = require("../models/characterSchema");
+const { getAll, getOne } = require("../modules/getRequests");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const characters = await Character.find().then((result) => {
-      res.json({ characters: result });
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  const allCharacters = await getAll(req, res, Character);
+  res.json(allCharacters);
 });
 
 router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  let character;
-  try {
-    const character = await Character.findById(id).then((result) => {
-      res.json({ character: result });
-    });
-  } catch (err) {
-    if (character == null) {
-      res.json({ message: "Cannot find character" });
-    } else {
-      res.json({ message: err.message });
-    }
-  }
+  const character = await getOne(req, res, req.params.id, Character);
+  res.json(character);
 });
 
 module.exports = router;
