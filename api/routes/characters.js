1fc36b462a7049eb30f8646b55e625de237454cc
dmychel <dmychel@tutanota.com>
2024-03-28 16:17:53 -0400

    Get requests for characters
    Two kinds of GET requests, one is to request data on all characters, the second is to request data on a specific character based on an ID.
    
    I will eventually move these functions into their own modules as they will be useful for other requests

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -1,18 +1,32 @@
 const express = require("express");
+const Character = require("../models/characterSchema");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Handling GET request /character",
-  });
+router.get("/", async (req, res) => {
+  try {
+    const characters = await Character.find().then((result) => {
+      res.json({ characters: result });
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.get("/:characterId", (req, res) => {
-  const id = req.params.characterId;
-  res.status(200).json({
-    message: "you passed a character ID",
-  });
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  let character;
+  try {
+    const character = await Character.findById(id).then((result) => {
+      res.json({ character: result });
+    });
+  } catch (err) {
+    if (character == null) {
+      res.json({ message: "Cannot find character" });
+    } else {
+      res.json({ message: err.message });
+    }
+  }
 });
 
 module.exports = router;
