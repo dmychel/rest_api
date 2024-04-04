d51666f4aa2e5504c12692f27cdea41821ddfde8
dmychel <dmychel@tutanota.com>
2024-04-04 13:27:09 -0400

    imported and call randomNum function

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Character = require("../models/characterSchema");
 const { getAll, getOne } = require("../modules/getRequests");
+const { randomNum } = require('../modules/randomNum')
 
 const router = express.Router();
 
@@ -9,15 +10,12 @@ router.get("/all", async (req, res) => {
 });
 
 router.get("/random", async (req, res) => {
-  const id = Math.floor(Math.random() * 23)
-  const character = await getOne(req, res, id, Character);
+  const id = randomNum(24)
+  await getOne(req, res, id, Character);
 });
-
 
 router.get("/:id", async (req, res) => {
   const character = await getOne(req, res, req.params.id, Character);
 });
-
-
 
 module.exports = router;
